2964f14ac58bb4a5b426b1df2a1ae59018ed09e2
AnzeBricman <anze.bricman2@gmail.com>
2026-01-18 21:57:00 +0100

    poprava tailwind css

--- app/(app)/layout.tsx
+++ app/(app)/layout.tsx
@@ -1,5 +1,9 @@
 import AppShell from "@/components/layout/app-shell";
 
-export default function AppLayout({ children }: { children: React.ReactNode }) {
+export default function AppLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return <AppShell>{children}</AppShell>;
 }
